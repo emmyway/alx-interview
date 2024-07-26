754da0584247ff327922eef0caf689e3586a8359
emmanuel nnaji <realreach@yahoo.com>
2024-07-26 04:45:17 +0100

    fxn returns a list of lists of integers representing the Pascal’s triangle of n

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,33 @@
+def pascal_triangle(n):
+    '''
+    Generate a Pascal's triangle up to nth row.
+
+    Args:
+    n (int): The number of rows in Pascal's triangle to generate.
+
+    Returns:
+    List[List[int]]: A list of lists representing Pascal's triangle, Return an empty list
+    '''
+    if n <= 0:
+        return[]
+
+    # Initialize Pascal's triangle withthe first row
+    triangle = [[1]]
+
+    # construct each row of Pascal's triangle
+    for i in range(1, n):
+        # start the row with a 1
+        row = [1]
+
+        # compute the values for the current row
+        for j in range(1, i):
+            # each value is the sum of the two values directly above it
+            row.append(triangle[i-1][j-1]+ triangle[i-1][j])
+
+        # end the row with a 1
+        row.append(1)
+
+        # Add the row to the triangle
+        triangle.append(row)
+
+    return triangle
---
+++ 0x00-pascal_triangle/README.md
@@ -0,0 +1,4 @@
+function def pascal_triangle(n): that returns a list of lists of integers representing the Pascal’s triangle of n:
+
+Returns an empty list if n <= 0
+assume n will be always an integer
