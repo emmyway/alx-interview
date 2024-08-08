414a6f8ac4013c77763be6447fec7215983657ee
emmanuel nnaji <realreach@yahoo.com>
2024-08-08 17:02:07 +0100

    debugged to meet pycodestyle

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -9,17 +9,17 @@ def minOperations(n):
     """
     Calculate the minimum number of operations to
     get exactly n 'H' characters.
-    
+ 
     Args:
     n (int): The target number of 'H' characters.
-    
+
     Returns:
     int: Minimum number of operations to achieve exactly
     n 'H' characters. Returns 0 if n is not achievable.
     """
     if n <= 1:
         return 0
-    
+
     operations = 0
     factor = 2
 
